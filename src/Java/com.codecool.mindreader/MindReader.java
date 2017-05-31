96b4d3e1b4d8770ff0bb032630086bb435cd627f
Balázs Máté <balazsm86@gmail.com>
2017-05-31 11:15:51 +0200

    Initializing classes and test files

---
+++ src/Java/com.codecool.mindreader/MindReader.java
@@ -0,0 +1,26 @@
+package com.codecool.mindreader;
+
+/**
+ * Created by mate on 2017.05.31..
+ */
+public class MindReader {
+    private void newTurn(int numOfCorrectDigits) {
+
+    }
+
+    private void newDigit() {
+
+    }
+
+    private void newNumber() {
+
+    }
+
+    private void compareNumbers() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Java/com.codecool.mindreader/Number.java
@@ -0,0 +1,18 @@
+package com.codecool.mindreader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by mate on 2017.05.31..
+ */
+public class Number {
+    private Stack listOfDigits;
+    private List<ArrayList> indicesOfCorrectDigits;
+
+    public int getIntNumber() {
+        int number = 0;
+        return number;
+    }
+}
