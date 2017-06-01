8deb53386390aca174f7a59a76c4b9c4463f0394
Balázs Máté <balazsm86@gmail.com>
2017-06-01 20:40:25 +0200

    Adding JavaDoc to the classes and methods

--- src/Java/com.codecool.mindreader/MindReader.java
+++ src/Java/com.codecool.mindreader/MindReader.java
@@ -3,6 +3,11 @@ package com.codecool.mindreader;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This class takes the number of digits (<tt>numberOfDigits</tt>) from the user, and returns with a random number,
+ * then asks the user, how many digits is in correct place. The user can only input this one parameter, and the
+ * program tries to find the guessed number.
+ */
 public class MindReader {
     private Number guessedNumber;
     private int numberOfDigits;
@@ -12,19 +17,40 @@ public class MindReader {
         this.setup();
     }
 
-    private void setup() {
+    /**
+     * This method generates a new random number, and stores as Number type.
+     */
+    public void setup() {
         guessedNumber = newNumber();
     }
 
-    private void newTurn() {
+    /**
+     * This method runs until the program finds the guessed number.
+     */
+    public void newTurn() {
         int correctDigits = userInput();
         while (correctDigits != numberOfDigits) {
-            guessedNumber = newNumber();
+            compareNumbers();
             correctDigits = userInput();
         }
     }
 
-    private Number newNumber() {
+    /**
+     * This method generates a new random Number, and compares with the previous number. If the number of correct digits
+     * greater than previously, it sets the new number as the new guessed number.
+     */
+    public void compareNumbers() {
+        Number newNumber = newNumber();
+        if (newNumber.getNumOfCorrectDigits() > guessedNumber.getNumOfCorrectDigits()) {
+            guessedNumber = newNumber;
+        }
+    }
+
+    /**
+     * This method generates new Number from random digits.
+     * @return Returns with the generated Number.
+     */
+    public Number newNumber() {
         Number newNumber = new Number();
         for (int i = 0; i < numberOfDigits; i++) {
             newNumber.addDigitToList(newDigit(i));
@@ -32,26 +58,29 @@ public class MindReader {
         return newNumber;
     }
 
-    private void compareNumbers(Number number) {
-
-    }
-
-    private void mostCorrectNumber(Number number) {
-        if (number.getNumOfCorrectDigits() > guessedNumber.getNumOfCorrectDigits()) {
-            guessedNumber = number;
-        }
-    }
-
+    /**
+     * This method generates a new int digit 0-9 or 1-9, depending from the given <tt>index</tt> parameter.
+     * @param index If <tt>index</tt> is zero (first digit of the number), the new digit is 1-9, otherwise is 0-9.
+     * @return Returns with the generated digit.
+     */
     public int newDigit(int index) {
         return (index == 0) ? new Random().nextInt(9) : new Random().nextInt(8) + 1;
     }
 
-    private int userInput() {
+    /**
+     * This method takes the number of correct digits from the user.
+     * @return Returns with the integer user input.
+     */
+    public int userInput() {
         System.out.println("Number of correct digits: ");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 
+    /**
+     * This is the main method of the program.
+     * @param args List of arguments.
+     */
     public static void main(String[] args) {
         System.out.println("Digits of number: ");
         Scanner scanner = new Scanner(System.in);
--- src/Java/com.codecool.mindreader/Number.java
+++ src/Java/com.codecool.mindreader/Number.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * This class implements the generated number. The number is stored as a list of digits. Another list contains
+ * information about the digits' correctness.
+ */
 public class Number {
     private List<Integer> listOfDigits;
     private List<Integer> indicesOfCorrectDigits;
@@ -13,22 +17,42 @@ public class Number {
         this.indicesOfCorrectDigits = new ArrayList<>();
     }
 
+    /**
+     * This method adds a digit to the digit list.
+     * @param digit Generated random digit.
+     */
     public void addDigitToList(int digit) {
         listOfDigits.add(digit);
     }
 
+    /**
+     * This method adds the index of the correct digit to the index list.
+     * @param index Index of the correct digit in the digit list.
+     */
     public void addIndexToCorrectDigitsList(int index) {
         indicesOfCorrectDigits.add(index);
     }
 
+    /**
+     * This method get the list of indices.
+     * @return Returns with the list of indices.
+     */
     public List<Integer> getIndicesOfCorrectDigits() {
         return indicesOfCorrectDigits;
     }
 
+    /**
+     * This method get the length of the index list.
+     * @return Returns with the length of the index list
+     */
     public int getNumOfCorrectDigits() {
         return indicesOfCorrectDigits.size();
     }
 
+    /**
+     * This method creates an integer number from the digit list.
+     * @return Returns the integer number.
+     */
     public int getIntNumber() {
         StringBuilder stringBuilder = new StringBuilder();
         for (Integer digit : listOfDigits) {
