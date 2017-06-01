2468451bbf3bb5574439d297eada5ab980509d55
Balázs Máté <balazsm86@gmail.com>
2017-06-01 11:54:16 +0200

    Generating new first or not first digit

--- src/Java/com.codecool.mindreader/MindReader.java
+++ src/Java/com.codecool.mindreader/MindReader.java
@@ -1,15 +1,21 @@
 package com.codecool.mindreader;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by mate on 2017.05.31..
  */
 public class MindReader {
+    private List<Number> numbers = new ArrayList<>();
+
     private void newTurn(int numOfCorrectDigits) {
 
     }
 
-    private void newDigit() {
-
+    public int newDigit(int index) {
+        return (index == 0) ? new Random().nextInt(9) : new Random().nextInt(8) + 1;
     }
 
     private void newNumber() {
